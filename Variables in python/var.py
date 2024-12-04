80b20d8344ed57dfb1dc9163b81d3a6cfdd240b0
imabuzerkhan <abuzerk41@gmail.com>
2024-12-04 16:55:31 +0545

    variable completed

---
+++ Variables in python/var.py
@@ -0,0 +1,20 @@
+# so lets  talk about variables in python 
+
+# name = "abuzer"
+# age = 22
+# price =22.43
+# print(age)
+# print(price)
+# print(name)
+
+
+# as you see in a example (name ) is variable and the date is a value 
+
+
+# how to checck the type of datatypes in python
+name = "bandr sing"
+age = 22 
+price = 34.56
+print (type(price))
+print(type(age))
+print(type( name ))
