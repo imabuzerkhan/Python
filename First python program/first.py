65af0f54170ea74888ab9bb115138dc6b5011b72
imabuzerkhan <abuzerk41@gmail.com>
2024-12-04 16:48:09 +0545

    first python program

---
+++ First python program/first.py
@@ -0,0 +1,5 @@
+# first python program
+
+print("hello abuzer khan ")
+print("hello world")
+print("hello world" , "it is my first python code" , "and i am very happy to start my journey with python progamming language")
