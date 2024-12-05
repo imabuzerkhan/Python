77469e273d558cfb58b17408b958acb7ac244f25
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 18:29:31 +0545

    new commit

---
+++ 3. datatypes/Assignment.py
@@ -0,0 +1,13 @@
+# assignment operator
+"""
+in assignment operator we can simply add the value of in existing value
+
+"""
+firstvalue = 10
+# firstvalue += 20
+# firstvalue *= 10
+# firstvalue -= 5
+# firstvalue /= 2
+# firstvalue %= 5
+# firstvalue **=2
+print(firstvalue)
