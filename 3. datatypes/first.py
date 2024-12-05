6e777349e0068a453e059d95cf3f15b5cc991445
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 18:38:19 +0545

    new commit

---
+++ 3. datatypes/first.py
@@ -0,0 +1,19 @@
+# datatypes in python programming
+# basically there is 5 primitive data types in python
+
+# ? Integer
+a = 23
+
+# ? float
+b =23.4
+
+# ? boolean
+(True , False)
+
+# ? String
+a = "abuzer khan"
+
+# ? None
+a =none
+
+# none ka mtlb hota hai kii variable ko khali rakhna hai 
