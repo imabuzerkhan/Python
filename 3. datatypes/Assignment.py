ac55daea6a286000f697d2e51489062558dffd81
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 18:33:10 +0545

    new commit

--- 3. datatypes/Assignment.py
+++ 3. datatypes/Assignment.py
@@ -11,3 +11,16 @@ firstvalue = 10
 # firstvalue %= 5
 # firstvalue **=2
 print(firstvalue)
+
+#  another example
+a = 10 
+b =3
+c =a+b
+print(c) # solving this problem using assignmet operator 
+
+a=10
+b=3
+a +=b
+print(a)
+
+# this how we store the value using assignment operator
