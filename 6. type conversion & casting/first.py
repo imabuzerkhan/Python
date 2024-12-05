772681371bb9786f04b350c9adf2a2d5ed5d859a
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 19:01:40 +0545

    type casting and type conversion

---
+++ 6. type conversion & casting/first.py
@@ -0,0 +1,19 @@
+#  sabse phele jante hai type conversion kya hota hai 
+
+"""
+basically type conversion wo conversion hai jo python automatically karta low data types ke value high data types ke value me store karta example dekhte hai 
+
+"""
+
+a = 34
+b= 23.6
+a += b
+print(a) 
+
+# ? ye jo example low value high dta types ke value me store karsakte hai 
+
+a = "23"
+b = 2
+b += int (a)
+print(b)
+
