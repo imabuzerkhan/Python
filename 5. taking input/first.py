01006d307382be7e45a314aa65488b7c7bc70d27
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 18:47:13 +0545

    taking input in python

---
+++ 5. taking input/first.py
@@ -0,0 +1,12 @@
+# ? how to take input in python program
+
+# ? input() ye input lene ka tariqa hai pyton aur ek baat jo imp hai kii agar hum iss tarah se input lenge to value hames string me aayega 
+
+# ? aur agar humhe int aur float me value chaiye to haan me tpe casting karna parega
+
+name = input("what is your name:")
+age = int(input("what is your age: "))
+marks = int(input("total marks"))
+print("my name is " , name, type(name)  )
+print("my age is " , age, type(age)  )
+print("my marks is " , marks, type(marks)  )
