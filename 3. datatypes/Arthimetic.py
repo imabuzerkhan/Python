d3f5c367ed64aa6f3211dfef2e192b86a6c85208
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 17:47:29 +0545

    Arthimetic done

---
+++ 3. datatypes/Arthimetic.py
@@ -0,0 +1,28 @@
+#  arthimetic operator 
+a = 23
+b=34
+sum = a+b
+pro = a *b 
+mul = a*b
+power = a**b
+div = a/b
+sub = a - b
+reminder = a % b
+print ("the sum of two num is:" , sum )
+print ("the pro of two num is:" , pro )
+print ("the sub of two num is:" , sub )
+print ("the power of two num is:" , power )
+print ("the div of two num is:" , div )
+print ("the reminder of two num is:" , reminder )
+
+# * output of the code
+
+""" (itis a multi line comment)
+the sum of two num is: 57
+the pro of two num is: 782
+the sub of two num is: -11
+the power of two num is: 19895113660064588580108197261066338165074766609
+the div of two num is: 0.6764705882352942
+the reminder of two num is: 23
+
+"""
