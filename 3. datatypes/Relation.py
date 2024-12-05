f71177c9322d8cb1746cade971e1cf89d4eeb9f9
imabuzerkhan <abuzerk41@gmail.com>
2024-12-05 17:52:47 +0545

    relationoperator

---
+++ 3. datatypes/Relation.py
@@ -0,0 +1,27 @@
+#  relation /comparison operator
+
+a =35
+b=23
+equal = a==b
+graterthan = a>b
+lessthan = a<b
+lessthanequalto = a<=b
+graterthanequal = a>=b
+notequalto = a!=b
+print(equal)
+print(graterthan)
+print(lessthan)
+print(lessthanequalto)
+print(graterthanequal)
+print(notequalto)
+
+# ** output of the code is 
+
+"""
+False
+True
+False
+False
+True
+True
+"""
