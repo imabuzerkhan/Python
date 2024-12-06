89a45110fc4839c93ed853c882fb5b712e826a7c
imabuzerkhan <abuzerk41@gmail.com>
2024-12-06 16:38:04 +0545

    indexing

---
+++ 7.String/IndexingStr.py
@@ -0,0 +1,15 @@
+# lets talk about the index in string what is it and how its work so lets discuss about postive index 
+# ? it always start 0 in python
+
+name = "Mohammad"
+index = name[4]
+
+# ? can we assign the new valuue to the index no its possible 
+#! name[4] = p
+print("the index of 2 no is what:" , index)
+
+# ? we can learn negative index
+# ? its start from last and -1 not from 0
+name = 'danish'
+indexn = name[-1]
+print(indexn)
