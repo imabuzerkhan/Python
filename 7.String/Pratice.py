7cefcf0802ddb113437420aa23bc90f1f1d645b2
imabuzerkhan <abuzerk41@gmail.com>
2024-12-06 16:53:34 +0545

    pratice just two question

---
+++ 7.String/Pratice.py
@@ -0,0 +1,11 @@
+# ? 
+# ! wap to input from user and show its length
+
+# name = input("what is your name: please tell us:")
+# print(len(name))
+
+# ! wap to S in String
+
+name = "String"
+find1 = name.find("S")
+print(find1)
