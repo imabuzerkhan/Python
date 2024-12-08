5a7cf0c446b77a396dba3e508ef453f7ca1401e1
imabuzerkhan <abuzerk41@gmail.com>
2024-12-08 08:12:53 +0545

    if else

---
+++ Conditional statement/First.py
@@ -0,0 +1,19 @@
+# if statement 
+
+
+# age = 24
+# if age>=24:
+#   print("it is not eligible for vote")
+
+#   print("program ended")
+
+# ! if else statement 
+
+age = 18
+
+if age>=18:
+  print("it is eleigble for vote")
+else :
+    print("it is not eleible for vote")
+
+    print("program ended here ")
