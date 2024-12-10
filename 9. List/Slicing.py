86a3d98badc1f42f64071a96a52cb339063e9eb8
imabuzerkhan <abuzerk41@gmail.com>
2024-12-10 17:45:03 +0545

    new commit

--- 9. List/Slicing.py
+++ 9. List/Slicing.py
@@ -1,3 +1,13 @@
 # slicing in list is possible
 
 name = ["good" , "very good" , "nice"]
+store = name[0:2]
+print(store)
+
+num = [1,2,3,4,5,6,7,5,3,2,1,2,4,3,1,12,322,41,]
+numStore = num[:5]
+print(numStore)
+
+
+slice = ['dsffjf' , 'dfsjfnjkasf' , 'fdfnjfn' , 'snjfnns']
+print(slice[2:])
