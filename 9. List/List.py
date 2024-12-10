c466460916517b05b18b5500b0b52ac296ebe23e
imabuzerkhan <abuzerk41@gmail.com>
2024-12-10 16:29:56 +0545

    list

---
+++ 9. List/List.py
@@ -0,0 +1,11 @@
+# list in python 
+# it is little bit similar to aaray 
+# it is bulit in and store different types of data 
+
+# ? first example of list
+
+marks = [90, 98.3 , 26 , 45 , 67]
+marks[2] = "Abuzer khan"  # it is possible to chnge the list data because it is mutable / changeable 
+print(marks)
+print(type(marks))
+print(len(marks))
