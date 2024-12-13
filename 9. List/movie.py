31bfb6f7d94df9e20dbc915794cc3e5a2082e069
imabuzerkhan <abuzerk41@gmail.com>
2024-12-13 09:53:06 +0545

    new commit for today

--- 9. List/movie.py
+++ 9. List/movie.py
@@ -4,5 +4,7 @@ movie = []
 movie.append(input("enter the first movie name"))
 movie.append(input("enter the first movie name"))
 movie.append(input("enter the first movie name"))
+movie.append(input("enter the first movie name"))
+
 
 print(movie)
