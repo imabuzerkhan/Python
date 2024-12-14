006781c82446b1426adff8f2fb2c25184217486b
imabuzerkhan <abuzerk41@gmail.com>
2024-12-14 06:40:24 +0545

    new commit

--- package/joke.py
+++ package/joke.py
@@ -1,4 +1,21 @@
+# import pyjokes
+
+# joke = pyjokes.get_jokes()
+# print(joke[0])
+
+
 import pyjokes
 
-joke = pyjokes.get_jokes()
-print(joke[0])
+# Function to get a joke based on theme
+def get_joke(category):
+    if category == "neutral":
+        joke = pyjokes.get_joke(category='neutral')
+    elif category == "chuck":
+        joke = pyjokes.get_joke(category='chuck')
+    else:
+        joke = "Sorry, I can only tell 'neutral' or 'chuck' jokes for now."
+    return joke
+
+# User input for joke type
+category = input("Enter the type of joke you want (neutral/chuck): ").strip().lower()
+print(get_joke(category))
