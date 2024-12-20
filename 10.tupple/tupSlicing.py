39b6b88b00e179732d0c94999d4d74dca6209ab3
imabuzerkhan <abuzerk41@gmail.com>
2024-12-20 09:41:50 +0545

    slicing

---
+++ 10.tupple/tupSlicing.py
@@ -0,0 +1,9 @@
+# ? slicing ka kuch example dekhte hai 
+
+tupple = ("we are from nepal")
+newvalue = tupple[2:]
+print(newvalue)
+#  ! ye slicing method hai jaise hum list me kam karte hai slicing metghod ka uss taths hum tupple me bhi karte hai same to samr 
+
+# * negative slicing dekhte hai ek bar 
+
