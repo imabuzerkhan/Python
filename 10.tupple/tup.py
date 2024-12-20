7d80c0942b7de18c4236e8688704bed674b54ae6
imabuzerkhan <abuzerk41@gmail.com>
2024-12-20 09:38:27 +0545

    practing tupple question

---
+++ 10.tupple/tup.py
@@ -0,0 +1,23 @@
+# what is tupple and how it work lets see
+tup = () 
+print(tup)
+print(type(tup))
+
+# ? tupple or list bilkul same hots hai bas ek difference ye hai list change able hai aur tupple changeable nhi hai
+
+tupple = (1,2 ,3 ,"abuzer", "baker")
+print(tupple)
+print(len(tupple))
+
+
+# ? agar tupple sirf ek hii value dalenge jaise kii one aur bina (') comma se seprate to wo value ko integer samaj jayega 
+
+value = (1)
+print(value)
+print(type(value))  #! app ne dekha kii jaise humne tupple me koi single value dfala magar usko comma se seprsatwe nhi kiya to usne integer le liye wo value ko 
+
+# ? aab comma se seprate karke dekhte hai 
+
+item = ('one' , )
+print(type(item))
+print(item)
