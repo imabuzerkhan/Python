8fe62889828c9b95f4c3ae2060c5688a1f39a985
imabuzerkhan <abuzerk41@gmail.com>
2024-12-20 15:16:39 +0545

    tupple method

---
+++ 10.tupple/tupplemthod.py
@@ -0,0 +1,13 @@
+# ! tupple method
+
+# ! count()
+tupple = ("mr abuzer khan i am from biratnagr neepal")
+patalagna = tupple.count("i")
+print(patalagna)
+
+
+# ! index()
+
+newTupple = ("hello mr khan")
+indexno = newTupple.index("r")
+print(indexno)  
