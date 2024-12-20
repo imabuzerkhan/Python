1dd999b03c64d87c6dce95ba620007a849bb1a33
imabuzerkhan <abuzerk41@gmail.com>
2024-12-20 15:25:21 +0545

    first programming

---
+++ 11. dictionary/first.py
@@ -0,0 +1,19 @@
+#  what is dictionary
+
+# ! for example in other language we say object 
+
+object = {
+  "name" : "abuzer",
+  "age" : 23 ,
+  "father's name" : "shalauddin khan" ,
+  "mother name" : "rehan khatoon"
+}
+
+print(object)
+access = object["name"]
+print(access)
+
+# ! also we used get() function to access name age etc
+
+fathername = object.get("father's name")
+print(fathername)
