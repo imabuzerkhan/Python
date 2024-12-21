8af07ecd6619012199576326dd0603be218f6d1b
imabuzerkhan <abuzerk41@gmail.com>
2024-12-21 20:01:57 +0545

    while loop

---
+++ 13.loops/while.py
@@ -0,0 +1,15 @@
+# while loop
+
+# ! print hello world 10 times for using while loop
+
+# count = 1
+# while count<=10 :
+#   print("hello world")
+#   count += 1 
+
+  # ! print 1 to 100 number for using while loops
+
+count = 1
+while count<=100:
+  print("the no is" , count)
+  count +=1
