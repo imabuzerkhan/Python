d9faf76059d925ba90feeed4dbb668547bffcdf8
imabuzerkhan <abuzerk41@gmail.com>
2024-12-23 22:31:18 +0545

    for loop

---
+++ 13.loops/forloop.py
@@ -0,0 +1,15 @@
+# for  loop 
+
+# ! first example of for for loop
+
+list = {"abuzer ", 2,4,3,2,1}
+
+for item in list:
+  print(item)
+
+  # ! second example 
+ # Example of a for loop with a tuple
+vegetable = ("aalu", "sag", "sabjii", "papita")
+
+for item in vegetable:
+    print(item)
