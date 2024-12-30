2b353e4039ed5381a153ce3ad856a59a80924003
imabuzerkhan <abuzerk41@gmail.com>
2024-12-30 16:36:05 +0545

    even number

--- 13.loops/Practice2.py
+++ 13.loops/Practice2.py
@@ -1,5 +1,5 @@
 # wap to print square of number 1 to 25
 
-for number in range (1, 26):
+for number in range (1, 26 , 2):
   Square = number ** 2 
   print( Square , end=" " )
---
+++ 13.loops/pratice3.py
@@ -0,0 +1,5 @@
+# print even number 1 to 10 
+
+for Num in range(1 , 101):
+  if(Num % 2 == 0):
+    print("it is  even number " , Num)
