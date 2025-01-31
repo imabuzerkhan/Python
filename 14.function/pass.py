dfcc2cd2c74e4e9b24f3e173fdc40880e572126a
imabuzerkhan <abuzerk41@gmail.com>
2025-01-31 20:02:43 +0545

    pass in python

---
+++ 14.function/pass.py
@@ -0,0 +1,12 @@
+def num(num1):
+   pass     # its prevent from error in python 
+ 
+ 
+print("Function defined but does nothing yet.")
+
+
+age = 18 
+if (age >= 18):
+   pass 
+else :
+   print("age match nhi huwa hai")
