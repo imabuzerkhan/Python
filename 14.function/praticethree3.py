e5b6ed8306c12d052679f6efb7de14e9038d7da2
imabuzerkhan <abuzerk41@gmail.com>
2025-01-31 19:28:22 +0545

    string and number multiply

---
+++ 14.function/praticethree3.py
@@ -0,0 +1,6 @@
+def NumberStr(string , num) :
+  return string * num
+  # return string + num     
+  # ! not possible to add string and number in python 
+result = NumberStr("h" , 5)   
+print(result)
