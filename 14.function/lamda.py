34b53602b0fb3265973beaba0828067a00f64fb8
imabuzerkhan <abuzerk41@gmail.com>
2025-01-31 19:52:17 +0545

    lambda with two parameter

---
+++ 14.function/lamda.py
@@ -0,0 +1,10 @@
+#  practicing lambda
+cell = lambda x : x + 4 
+result = cell(2)
+print(result)
+# print(cell(2))
+
+#  lambda with two parameter
+sum = lambda a , b : a * b + a
+result2 = sum(2 , 5)
+print(result2)
